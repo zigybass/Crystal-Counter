72a108561a6782a48709184c9bad89354f9242d3
Zigy <zigybass@gmail.com>
2019-07-22 18:55:52 -0400

    added state variables. game reset function works. PseudoCode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,3 +1,36 @@
+$(document).ready(function () {
+
+
 let counter = 0;
 let wins = 0;
 let losses = 0;
+let gameWin = false;
+
+let emerald = 0;
+let ruby = 0;
+let sapphire = 0;
+let topaz = 0;
+let targetNum = 0;
+
+function gameReset () {
+    counter = 0;
+    let emerald = (Math.floor(Math.random() * 12 + 1));
+    let ruby = (Math.floor(Math.random() * 12 + 1));
+    let sapphire = (Math.floor(Math.random() * 12 + 1));
+    let topaz = (Math.floor(Math.random() * 12 + 1));
+    console.log(emerald, ruby, sapphire, topaz)
+    gameWin = false;
+}
+
+
+// reset game
+// assign gems/target randoms
+// listen for clicks on buttons
+// create random target score
+// add score when button is clicked
+// compare target with counter:
+//      if counter goes over, add loss, reset
+//      if counter matches, add win, reset
+// Create new random numbers when reset
+
+});
